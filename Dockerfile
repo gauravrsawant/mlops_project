55147e4150befe3fa4efc89572f97a2bb8ca53e7
GauravWin <gauravrsawant1313@gmail.com>
2024-04-26 01:38:17 +0530

    final commit

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+# Use an official Python runtime as a parent image
+FROM python:3.7.9
+
+# Set the working directory in the container
+WORKDIR /usr/src/app
+
+# Copy the current directory contents into the container at /usr/src/app
+COPY . .
+
+# Install any needed dependencies specified in requirements.txt
+RUN pip install --no-cache-dir -r requirements.txt
+
+# Expose the port that Flask is running on
+EXPOSE 5000
+
+# Define environment variable
+ENV FLASK_APP=app.py
+
+# Run app.py when the container launches
+CMD ["flask", "run", "--host=0.0.0.0", "--port=5000"]
---
+++ app.py
@@ -0,0 +1,143 @@
+import os
+from flask import Flask, render_template, request
+import pandas as pd
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.feature_extraction.text import TfidfVectorizer
+from textblob import TextBlob
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+import json
+
+# app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
+songDF = pd.read_csv("./data1/allsong_data.csv")
+complete_feature_set = pd.read_csv("./data1/complete_feature.csv")
+#create an instance of a flask app and indicating its location
+app_path = os.path.dirname(__file__)
+app = Flask(__name__, static_folder=app_path+'/static')
+#creating a secret key for security
+app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba241'
+
+def extract(URL):
+    # client_id = "5356afb958c84e71a2c37c43e2a2cbf2" 
+    # client_secret = "83e531491e9c458ba658ac30c4c56bc0"
+    client_id = 'f17085dc406f4736811fb37b3990077c'
+    client_secret = '15b9da1fa07e4fc19d7289ceac8e1b39'
+    #use the clint secret and id details
+    client_credentials_manager = SpotifyClientCredentials(client_id=client_id,client_secret=client_secret)
+    sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+
+    # the URI is split by ':' to get the username and playlist ID
+    playlist_id = URL.split("/")[4].split("?")[0]
+    playlist_tracks_data = sp.playlist_tracks(playlist_id)
+
+    #lists that will be filled in with features
+    playlist_tracks_id = []
+    playlist_tracks_titles = []
+    playlist_tracks_artists = []
+    playlist_tracks_first_artists = []
+
+    #go through the dictionary to extract the data
+    for track in playlist_tracks_data['items']:
+        playlist_tracks_id.append(track['track']['id'])
+        playlist_tracks_titles.append(track['track']['name'])
+        # adds a list of all artists involved in the song to the list of artists for the playlist
+        artist_list = []
+        for artist in track['track']['artists']:
+            artist_list.append(artist['name'])
+        playlist_tracks_artists.append(artist_list)
+        playlist_tracks_first_artists.append(artist_list[0])
+
+    #create a dataframe
+    features = sp.audio_features(playlist_tracks_id)
+    features_df = pd.DataFrame(data=features, columns=features[0].keys())
+    features_df['title'] = playlist_tracks_titles
+    features_df['first_artist'] = playlist_tracks_first_artists
+    features_df['all_artists'] = playlist_tracks_artists
+    features_df = features_df[['id', 'title', 'first_artist', 'all_artists',
+                                'danceability', 'energy', 'key', 'loudness',
+                                'mode', 'acousticness', 'instrumentalness',
+                                'liveness', 'valence', 'tempo',
+                                'duration_ms', 'time_signature']]
+    
+    return features_df
+
+def generate_playlist_feature(complete_feature_set, playlist_df):
+    '''
+    Summarize a user's playlist into a single vector
+    ---
+    Input: 
+    complete_feature_set (pandas dataframe): Dataframe which includes all of the features for the spotify songs
+    playlist_df (pandas dataframe): playlist dataframe
+        
+    Output: 
+    complete_feature_set_playlist_final (pandas series): single vector feature that summarizes the playlist
+    complete_feature_set_nonplaylist (pandas dataframe): 
+    '''
+    
+    # Find song features in the playlist
+    complete_feature_set_playlist = complete_feature_set[complete_feature_set['id'].isin(playlist_df['id'].values)]
+    # Find all non-playlist song features
+    complete_feature_set_nonplaylist = complete_feature_set[~complete_feature_set['id'].isin(playlist_df['id'].values)]
+    complete_feature_set_playlist_final = complete_feature_set_playlist.drop(columns = "id")
+    return complete_feature_set_playlist_final.sum(axis = 0), complete_feature_set_nonplaylist
+
+
+def generate_playlist_recos(df, features, nonplaylist_features):
+    '''
+    Generated recommendation based on songs in aspecific playlist.
+    ---
+    Input: 
+    df (pandas dataframe): spotify dataframe
+    features (pandas series): summarized playlist feature (single vector)
+    nonplaylist_features (pandas dataframe): feature set of songs that are not in the selected playlist
+        
+    Output: 
+    non_playlist_df_top_40: Top 40 recommendations for that playlist
+    '''
+    
+    non_playlist_df = df[df['id'].isin(nonplaylist_features['id'].values)]
+    # Find cosine similarity between the playlist and the complete song set
+    non_playlist_df['sim'] = cosine_similarity(nonplaylist_features.drop('id', axis = 1).values, features.values.reshape(1, -1))[:,0]
+    non_playlist_df_top_40 = non_playlist_df.sort_values('sim',ascending = False).head(40)
+    
+    return non_playlist_df_top_40
+
+
+def recommend_from_playlist(songDF,complete_feature_set,playlistDF_test):
+
+    # Find feature
+    complete_feature_set_playlist_vector, complete_feature_set_nonplaylist = generate_playlist_feature(complete_feature_set, playlistDF_test)
+    
+    # Generate recommendation
+    top40 = generate_playlist_recos(songDF, complete_feature_set_playlist_vector, complete_feature_set_nonplaylist)
+
+    return top40
+
+@app.route("/")
+def home():
+   #render the home page
+   return render_template('home.html')
+
+@app.route("/about")
+def about():
+   #render the about page
+   return render_template('about.html')
+
+@app.route('/recommend', methods=['POST'])
+def recommend():
+   #requesting the URL form the HTML form
+   URL = request.form['URL']
+   #using the extract function to get a features dataframe
+   df = extract(URL)
+   #retrieve the results and get as many recommendations as the user requested
+   edm_top40 = recommend_from_playlist(songDF, complete_feature_set, df)
+   number_of_recs = int(request.form['number-of-recs'])
+   my_songs = []
+   for i in range(number_of_recs):
+      my_songs.append([str(edm_top40.iloc[i,1]) + ' - '+ '"'+str(edm_top40.iloc[i,4])+'"', "https://open.spotify.com/track/"+ str(edm_top40.iloc[i,-6]).split("/")[-1]])
+   return render_template('results.html',songs= my_songs)
+
+if __name__ == '__main__':
+	app.run(debug=True)
